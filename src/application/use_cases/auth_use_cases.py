9922ac7aee7510f7017817cf86a7f054dcbc3803
NormanwOw <normjkeee@vk.com>
2025-05-19 23:03:04 +0500

    ADDED settings to AuthUser

--- src/application/use_cases/auth_use_cases.py
+++ src/application/use_cases/auth_use_cases.py
@@ -1,8 +1,10 @@
+from asyncpg.pgproto.pgproto import timedelta
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import HTTPException
 
+from config import Settings
 from src.application.use_cases.base import UseCase
 from src.application.use_cases.user_use_cases import GetUserByEmail
+from src.domain.exceptions import AppException
 from src.domain.services.auth_service import AuthService
 from src.infrastructure.logger.interfaces import ILogger
 from src.presentation.routers.schemas import Token
@@ -10,19 +12,27 @@ from src.presentation.routers.schemas import Token
 
 class AuthUser(UseCase[OAuth2PasswordRequestForm, Token]):
 
-    def __init__(self, get_user: UseCase, auth_service: AuthService, logger: ILogger):
+    def __init__(
+        self,
+        get_user: UseCase,
+        auth_service: AuthService,
+        settings: Settings,
+        logger: ILogger
+    ):
         self.get_user: GetUserByEmail = get_user
         self.auth_service = auth_service
+        self.settings = settings
         self.logger = logger
 
     async def __call__(self, form_data: OAuth2PasswordRequestForm) -> Token:
         try:
             user = await self.get_user(form_data.username)
-            access_token = self.auth_service.get_access_token(form_data, user)
-
+            ttl = timedelta(minutes=self.settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+            access_token = self.auth_service.get_access_token(form_data, user, ttl)
             return Token(access_token=access_token)
-        except HTTPException as ex:
+        except AppException as ex:
             raise ex
-        except Exception:
+        except Exception as ex:
             self.logger.error(f'Ошибка при авторизации пользователя {form_data.username}')
-            raise HTTPException(status_code=500)
+            raise ex
+
